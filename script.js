f8df3bfc15fe862d8176ac446874db8c14413b16
Virgi Alpiansyah <virgialpiansyah20@gmail.com>
2026-01-17 07:41:03 +0700

    Final changes

--- script.js
+++ script.js
@@ -14,35 +14,30 @@ form.addEventListener("submit", function (e) {
   const hp = document.getElementById("hp").value.trim();
   const kategori = document.getElementById("kategori").value;
 
-  // validasi kosong
   if (nama === "" || email === "" || hp === "" || kategori === "") {
     pesan.textContent = "Semua data wajib diisi";
     pesan.style.color = "red";
     return;
   }
 
-  // validasi email
   if (!email.includes("@")) {
     pesan.textContent = "Email harus mengandung @";
     pesan.style.color = "red";
     return;
   }
 
-  // validasi nomor HP
   if (isNaN(hp)) {
     pesan.textContent = "Nomor HP hanya boleh angka";
     pesan.style.color = "red";
     return;
   }
 
-  // cek kuota
   if (totalPeserta >= maxPeserta) {
     pesan.textContent = "Pendaftaran ditutup, kuota sudah penuh";
     pesan.style.color = "red";
     return;
   }
 
-  // tambah peserta
   const li = document.createElement("li");
   li.textContent = nama + " - " + kategori;
   listPeserta.appendChild(li);
